0d0189d87ebbf93e187520833ea187767d9f08c6
Christina Mikheyeva <c.mikheyeva@gmail.com>
2024-02-20 11:45:02 +0100

    Fist task done

--- js/task-1.js
+++ js/task-1.js
@@ -1,81 +1,44 @@
-//----------------------- First option --------------------------------------
+const getUserNames = (users) => users.map(item => item.name)
 
-function isEnoughCapacity(products, containerSize) {
-    let sumProducts = 0;
-    for (const product in products) {
-        sumProducts += products[product]
-    }
-    return sumProducts < containerSize;
-}
 
 console.log(
-  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-); // true
+  getUserNames([
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    balance: 2764
+  },
+])
+); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
 
-console.log(
-  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-); // false
-
-console.log(
-  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
-); // true
-
-console.log(
-  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
-); // false
-
-//----------------------- Second option --------------------------------------
-
-// function isEnoughCapacity(products, containerSize) {
-//     const keys = Object.values(products);
-//     let total = 0;
-//     for (const number of keys) {
-//         total += number
-//     }
-//  return total > containerSize ? "false" : "true";
-// }
-
-// console.log(
-//   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-// ); // true
-
-// console.log(
-//   isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-// ); // false
-
-// console.log(
-//   isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
-// ); // true
-
-// console.log(
-//   isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
-// ); // false
-
-//----------------------- Third option --------------------------------------
-
-// function isEnoughCapacity(products, containerSize) {
-//     const keys = Object.keys(products);
-//     let total = 0;
-//     for (const number of keys) {
-//         total += products[number]
-//     }
-//  return total > containerSize ? "false" : "true";
-// }
-
-
-// console.log(
-//   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-// ); // true
-
-// console.log(
-//   isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-// ); // false
-
-// console.log(
-//   isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
-// ); // true
-
-// console.log(
-//   isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
-// ); // false
 
